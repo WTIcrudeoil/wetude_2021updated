eebd69bf1e1e0857dd26e3b6eb92a4f44f10b862
WTIcrudeoil <ldh7928@gmail.com>
2021-05-10 22:45:24 +0900

    Understanding of express routers"

--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
-This is a clone of youtube.
-This is a new version for 2021
-This is the third project using nodeJS
+# Wetube Reloaded
+
+어떠한 형태의 데이터를 다룰 것인지를 먼저 고려해야함
+이 프로젝트에서는 유저와 비디오가 메인이 될것이다.
+
+Router를 통해서 URL을 그룹화 하여 진행한다.
+/ -> Home
+/join -> join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -8,6 +8,25 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
+const globalRouter = express.Router();
+
+const handleHome =(req,res) => res.send("Home");
+
+globalRouter.get("/",handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req,res) => res.send("Edit User");
+
+userRouter.get("/edit",handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req,res) => res.send("Watch Video");
+
+videoRouter.get("/watch",handleWatchVideo);
+
+
 const routerLogger = (req,res,next) =>{
 next();
 }
@@ -16,7 +35,10 @@ const home = (req,res) => res.send("hello");
 
 
 app.use(logger);
-app.get("/",home);
+
+app.use("/",globalRouter);
+app.use("/videos",videoRouter);
+app.use("/users",userRouter);
 
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
