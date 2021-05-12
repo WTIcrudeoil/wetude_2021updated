f11cf9fbbd82bf0328d76b2e7b580136aaa87944
WTIcrudeoil <ldh7928@gmail.com>
2021-05-12 22:33:36 +0900

    how to use continals,iteration,mixins in pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,39 @@
-export const trending = (req,res) => res.render("home",{pageTitle:"Home"});
+const fakeUser ={
+    username:"Lee dong",
+    loggedIn:true,
+};
+
+export const trending = (req,res) => {
+    const videos = [
+        {
+            title:"First video",
+            rating:5,
+            comments:2,
+            createdAt:"2 hour ago",
+            views:59,
+            id:1,
+        },
+        {
+            title:"Second video",
+            rating:5,
+            comments:19,
+            createdAt:"2 hour ago",
+            views:19,
+            id:2,
+        },
+        {
+            title:"3 video",
+            rating:3.5,
+            comments:12,
+            createdAt:"2 hour ago",
+            views:9,
+            id:3,
+        },
+    ];
+    res.render("home",{pageTitle:"Home",fakeUser,videos});
+}
 export const see = (req,res) => res.render("watch");
-export const edit = (req,res) => {
-    console.log(req.params);
-    return res.send("Edit");}
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
 export const deleteVideo = (req,res) => {
