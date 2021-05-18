4c4b6b57c797ff41238e9ac85edb8cbd907fa946
WTIcrudeoil <ldh7928@gmail.com>
2021-05-18 22:50:08 +0900

    how to use your data from database - mongoose

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,8 +5,8 @@ const videoRouter = express.Router();
 
 videoRouter.get("/upload",getUpload);
 videoRouter.post("/upload",postUpload);
-videoRouter.get("/:id(\\d+)",watch);// ":"" let express know this is a variable
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit)
+videoRouter.get("/:id([0-9a-f]{24})",watch);// ":"" let express know this is a variable
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit)
 
 
 videoRouter.get("/:id(\\d+)/delete",deleteVideo);
